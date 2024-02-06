e66f155bb10637d19bccfc4d183956d03ffdc8a1
ogabrielrodrigues <gabrielrodrigues409@outlook.com>
2024-02-06 16:17:58 -0300

    add Data shortcut to map[string]interface{}

--- response.go
+++ response.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 )
 
+// Data is one shortcut to map[string]interface{}
+type Data map[string]interface{}
+
 // Write http status code and return data in JSON format
 func JSON(w http.ResponseWriter, code int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
